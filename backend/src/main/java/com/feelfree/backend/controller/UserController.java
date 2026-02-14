1f6e3f88568aad1334bbb76815eb95cfcfe5a727
ayushah08 <ayushah810@gmail.com>
2026-02-14 22:32:39 +0530

    Add User controller with basic CRUD endpoints

---
+++ backend/src/main/java/com/feelfree/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.feelfree.backend.controller;
+
+import com.feelfree.backend.entity.User;
+import com.feelfree.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
