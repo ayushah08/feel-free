8426e8a5c06db4adaebb788a421d12b157eec3c2
ayushah08 <ayushah810@gmail.com>
2026-02-14 21:58:57 +0530

    Add User entity and repository layer

---
+++ backend/src/main/java/com/feelfree/backend/entity/User.java
@@ -0,0 +1,50 @@
+package com.feelfree.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    private String profilePhotoUrl;
+
+    private int currentStreak;
+
+    private int longestStreak;
+
+    private int totalSupportGiven;
+
+    private int totalSupportTaken;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+}
